1081ed6ba6826e78bd26ac93adba8626692eac97
hchavezj <hchavezj@gmail.com>
2022-11-13 04:01:22 -0600

    added departments endpoint

--- src/routes/api/departments.js
+++ src/routes/api/departments.js
@@ -1,48 +1,51 @@
 import { Router } from 'express'
 
-import { getCompanies, getCompany, addCompany } from '../../models/companies'
-import { addDepartment } from '../../models/departments'
+import {
+  addDepartment,
+  getDepartment,
+  getDepartments,
+} from '../../models/departments'
 
 const router = Router()
 
 router.get('/', async (req, res) => {
-  const companies = await getCompanies()
-  res.send(companies)
+  const departments = await getDepartments()
+  res.send(departments)
 })
 
 router.get('/:id', async (req, res) => {
-  const company = await getCompany(req.params.id)
-  if (company) {
-    res.send(company)
+  const department = await getDepartment(req.params.id)
+  if (department) {
+    res.send(department)
   } else {
-    res.status(404).send({ msg: 'Company not found' })
+    res.status(404).send({ msg: 'Department not found' })
   }
 })
 
 router.post('/', async (req, res) => {
-  const companyName = req.body.name
-  if (companyName) {
-    const company = await addCompany(companyName)
-    res.send(company)
+  const departmentName = req.body.name
+  if (departmentName) {
+    const department = await addDepartment(departmentName)
+    res.send(department)
   } else {
-    res.status(400).send({ msg: 'Company name is required' })
+    res.status(400).send({ msg: 'Department name is required' })
   }
 })
 
-router.post('/:id', async (req, res) => {
-  const companyId = req.params.id
-  const company = await getCompany(companyId)
-  if (company) {
-    const departmentName = req.body.name
-    if (departmentName) {
-      const department = await addDepartment(departmentName, companyId)
-      res.send(department)
-    } else {
-      res.status(400).send({ msg: 'Department name is required' })
-    }
-  } else {
-    res.status(400).send({ msg: 'Company does not exist required' })
-  }
-})
+// router.post('/:id', async (req, res) => {
+//   const companyId = req.params.id
+//   const company = await getCompany(companyId)
+//   if (company) {
+//     const departmentName = req.body.name
+//     if (departmentName) {
+//       const department = await addDepartment(departmentName, companyId)
+//       res.send(department)
+//     } else {
+//       res.status(400).send({ msg: 'Department name is required' })
+//     }
+//   } else {
+//     res.status(400).send({ msg: 'Company does not exist required' })
+//   }
+// })
 
 export default router
